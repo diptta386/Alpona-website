2df131965ea8c14ec18da8ee66750b46c87470ca
diptta386 <dipttadebnath741@gmail.com>
2026-08-10 17:32:55 -0500

    Initialize Supabase client in supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,4 @@
 const SUPABASE_URL = "https://vicihroahvbkqyfkmwsv.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_9Lk0NkaHdH0mrnF7YL9W9Q_CIJYPIXX";
+const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
